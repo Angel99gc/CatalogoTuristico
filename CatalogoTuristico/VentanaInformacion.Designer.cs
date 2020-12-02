3f0af6e5febd441e21dace073ba30e0d707ef89e
Angel99gc <angel.gamboa12@hotmail.com>
2020-12-02 11:36:50 -0600

    Se crea la primer ventana con la informacion completa del lugar turistico

---
+++ CatalogoTuristico/VentanaInformacion.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace CatalogoTuristico
+{
+    partial class VentanaInformacion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NombreLabel = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.ReseñaLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LugarLabel = new System.Windows.Forms.Label();
+            this.TipoLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Autor1 = new System.Windows.Forms.Label();
+            this.Detalle1 = new System.Windows.Forms.Label();
+            this.Autor2 = new System.Windows.Forms.Label();
+            this.Autor3 = new System.Windows.Forms.Label();
+            this.Autor4 = new System.Windows.Forms.Label();
+            this.Autor5 = new System.Windows.Forms.Label();
+            this.Detalle2 = new System.Windows.Forms.Label();
+            this.Detalle3 = new System.Windows.Forms.Label();
+            this.Detalle4 = new System.Windows.Forms.Label();
+            this.Detalle5 = new System.Windows.Forms.Label();
+            this.BotonAtrasVentana = new System.Windows.Forms.Button();
+            this.BotonAnteriorComentario = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.IndexComentarioLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AutorTextBox = new System.Windows.Forms.TextBox();
+            this.ComentarioTextBox = new System.Windows.Forms.TextBox();
+            this.BotonAgregarComentario = new System.Windows.Forms.Button();
+            this.Icono = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NombreLabel
+            // 
+            this.NombreLabel.AutoSize = true;
+            this.NombreLabel.Location = new System.Drawing.Point(358, 40);
+            this.NombreLabel.Name = "NombreLabel";
+            this.NombreLabel.Size = new System.Drawing.Size(75, 13);
+            this.NombreLabel.TabIndex = 0;
+            this.NombreLabel.Text = "Estilo+Nombre";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(71, 96);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(34, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "Tipo: ";
+            // 
+            // ReseñaLabel
+            // 
+            this.ReseñaLabel.AutoSize = true;
+            this.ReseñaLabel.Location = new System.Drawing.Point(71, 138);
+            this.ReseñaLabel.Name = "ReseñaLabel";
+            this.ReseñaLabel.Size = new System.Drawing.Size(44, 13);
+            this.ReseñaLabel.TabIndex = 2;
+            this.ReseñaLabel.Text = "Reseña";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lugar: ";
+            // 
+            // LugarLabel
+            // 
+            this.LugarLabel.AutoSize = true;
+            this.LugarLabel.Location = new System.Drawing.Point(116, 246);
+            this.LugarLabel.Name = "LugarLabel";
+            this.LugarLabel.Size = new System.Drawing.Size(91, 13);
+            this.LugarLabel.TabIndex = 4;
+            this.LugarLabel.Text = "Provincia+Canton";
+            // 
+            // TipoLabel
+            // 
+            this.TipoLabel.AutoSize = true;
+            this.TipoLabel.Location = new System.Drawing.Point(112, 96);
+            this.TipoLabel.Name = "TipoLabel";
+            this.TipoLabel.Size = new System.Drawing.Size(54, 13);
+            this.TipoLabel.TabIndex = 5;
+            this.TipoLabel.Text = "TipoLabel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Comentarios:";
+            // 
+            // Autor1
+            // 
+            this.Autor1.AutoSize = true;
+            this.Autor1.Location = new System.Drawing.Point(130, 344);
+            this.Autor1.Name = "Autor1";
+            this.Autor1.Size = new System.Drawing.Size(38, 13);
+            this.Autor1.TabIndex = 7;
+            this.Autor1.Text = "Autor1";
+            // 
+            // Detalle1
+            // 
+            this.Detalle1.AutoSize = true;
+            this.Detalle1.Location = new System.Drawing.Point(209, 344);
+            this.Detalle1.Name = "Detalle1";
+            this.Detalle1.Size = new System.Drawing.Size(46, 13);
+            this.Detalle1.TabIndex = 8;
+            this.Detalle1.Text = "Detalle1";
+            // 
+            // Autor2
+            // 
+            this.Autor2.AutoSize = true;
+            this.Autor2.Location = new System.Drawing.Point(130, 379);
+            this.Autor2.Name = "Autor2";
+            this.Autor2.Size = new System.Drawing.Size(38, 13);
+            this.Autor2.TabIndex = 9;
+            this.Autor2.Text = "Autor2";
+            // 
+            // Autor3
+            // 
+            this.Autor3.AutoSize = true;
+            this.Autor3.Location = new System.Drawing.Point(128, 419);
+            this.Autor3.Name = "Autor3";
+            this.Autor3.Size = new System.Drawing.Size(38, 13);
+            this.Autor3.TabIndex = 10;
+            this.Autor3.Text = "Autor3";
+            // 
+            // Autor4
+            // 
+            this.Autor4.AutoSize = true;
+            this.Autor4.Location = new System.Drawing.Point(128, 458);
+            this.Autor4.Name = "Autor4";
+            this.Autor4.Size = new System.Drawing.Size(38, 13);
+            this.Autor4.TabIndex = 11;
+            this.Autor4.Text = "Autor4";
+            // 
+            // Autor5
+            // 
+            this.Autor5.AutoSize = true;
+            this.Autor5.Location = new System.Drawing.Point(128, 497);
+            this.Autor5.Name = "Autor5";
+            this.Autor5.Size = new System.Drawing.Size(38, 13);
+            this.Autor5.TabIndex = 12;
+            this.Autor5.Text = "Autor5";
+            // 
+            // Detalle2
+            // 
+            this.Detalle2.AutoSize = true;
+            this.Detalle2.Location = new System.Drawing.Point(209, 379);
+            this.Detalle2.Name = "Detalle2";
+            this.Detalle2.Size = new System.Drawing.Size(46, 13);
+            this.Detalle2.TabIndex = 13;
+            this.Detalle2.Text = "Detalle2";
+            // 
+            // Detalle3
+            // 
+            this.Detalle3.AutoSize = true;
+            this.Detalle3.Location = new System.Drawing.Point(209, 419);
+            this.Detalle3.Name = "Detalle3";
+            this.Detalle3.Size = new System.Drawing.Size(46, 13);
+            this.Detalle3.TabIndex = 14;
+            this.Detalle3.Text = "Detalle3";
+            // 
+            // Detalle4
+            // 
+            this.Detalle4.AutoSize = true;
+            this.Detalle4.Location = new System.Drawing.Point(209, 458);
+            this.Detalle4.Name = "Detalle4";
+            this.Detalle4.Size = new System.Drawing.Size(46, 13);
+            this.Detalle4.TabIndex = 15;
+            this.Detalle4.Text = "Detalle4";
+            // 
+            // Detalle5
+            // 
+            this.Detalle5.AutoSize = true;
+            this.Detalle5.Location = new System.Drawing.Point(209, 497);
+            this.Detalle5.Name = "Detalle5";
+            this.Detalle5.Size = new System.Drawing.Size(46, 13);
+            this.Detalle5.TabIndex = 16;
+            this.Detalle5.Text = "Detalle5";
+            // 
+            // BotonAtrasVentana
+            // 
+            this.BotonAtrasVentana.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BotonAtrasVentana.Location = new System.Drawing.Point(12, 12);
+            this.BotonAtrasVentana.Name = "BotonAtrasVentana";
+            this.BotonAtrasVentana.Size = new System.Drawing.Size(75, 23);
+            this.BotonAtrasVentana.TabIndex = 17;
+            this.BotonAtrasVentana.Text = "Atrás";
+            this.BotonAtrasVentana.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BotonAtrasVentana.UseVisualStyleBackColor = true;
+            // 
+            // BotonAnteriorComentario
+            // 
+            this.BotonAnteriorComentario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BotonAnteriorComentario.Location = new System.Drawing.Point(91, 548);
+            this.BotonAnteriorComentario.Name = "BotonAnteriorComentario";
+            this.BotonAnteriorComentario.Size = new System.Drawing.Size(75, 23);
+            this.BotonAnteriorComentario.TabIndex = 18;
+            this.BotonAnteriorComentario.Text = "Anterior";
+            this.BotonAnteriorComentario.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BotonAnteriorComentario.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(389, 548);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Siguiente";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // IndexComentarioLabel
+            // 
+            this.IndexComentarioLabel.AutoSize = true;
+            this.IndexComentarioLabel.Location = new System.Drawing.Point(220, 553);
+            this.IndexComentarioLabel.Name = "IndexComentarioLabel";
+            this.IndexComentarioLabel.Size = new System.Drawing.Size(112, 13);
+            this.IndexComentarioLabel.TabIndex = 20;
+            this.IndexComentarioLabel.Text = "IndexComentarioLabel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 620);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Autor: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 620);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Comentario: ";
+            // 
+            // AutorTextBox
+            // 
+            this.AutorTextBox.Location = new System.Drawing.Point(91, 617);
+            this.AutorTextBox.Name = "AutorTextBox";
+            this.AutorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AutorTextBox.TabIndex = 23;
+            // 
+            // ComentarioTextBox
+            // 
+            this.ComentarioTextBox.Location = new System.Drawing.Point(333, 617);
+            this.ComentarioTextBox.Multiline = true;
+            this.ComentarioTextBox.Name = "ComentarioTextBox";
+            this.ComentarioTextBox.Size = new System.Drawing.Size(238, 49);
+            this.ComentarioTextBox.TabIndex = 24;
+            // 
+            // BotonAgregarComentario
+            // 
+            this.BotonAgregarComentario.Location = new System.Drawing.Point(633, 610);
+            this.BotonAgregarComentario.Name = "BotonAgregarComentario";
+            this.BotonAgregarComentario.Size = new System.Drawing.Size(137, 23);
+            this.BotonAgregarComentario.TabIndex = 25;
+            this.BotonAgregarComentario.Text = "Agregar";
+            this.BotonAgregarComentario.UseVisualStyleBackColor = true;
+            // 
+            // Icono
+            // 
+            this.Icono.AutoSize = true;
+            this.Icono.Location = new System.Drawing.Point(69, 343);
+            this.Icono.Name = "Icono";
+            this.Icono.Size = new System.Drawing.Size(34, 13);
+            this.Icono.TabIndex = 26;
+            this.Icono.Text = "Icono";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 678);
+            this.Controls.Add(this.Icono);
+            this.Controls.Add(this.BotonAgregarComentario);
+            this.Controls.Add(this.ComentarioTextBox);
+            this.Controls.Add(this.AutorTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IndexComentarioLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BotonAnteriorComentario);
+            this.Controls.Add(this.BotonAtrasVentana);
+            this.Controls.Add(this.Detalle5);
+            this.Controls.Add(this.Detalle4);
+            this.Controls.Add(this.Detalle3);
+            this.Controls.Add(this.Detalle2);
+            this.Controls.Add(this.Autor5);
+            this.Controls.Add(this.Autor4);
+            this.Controls.Add(this.Autor3);
+            this.Controls.Add(this.Autor2);
+            this.Controls.Add(this.Detalle1);
+            this.Controls.Add(this.Autor1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TipoLabel);
+            this.Controls.Add(this.LugarLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ReseñaLabel);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.NombreLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NombreLabel;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label ReseñaLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LugarLabel;
+        private System.Windows.Forms.Label TipoLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Autor1;
+        private System.Windows.Forms.Label Detalle1;
+        private System.Windows.Forms.Label Autor2;
+        private System.Windows.Forms.Label Autor3;
+        private System.Windows.Forms.Label Autor4;
+        private System.Windows.Forms.Label Autor5;
+        private System.Windows.Forms.Label Detalle2;
+        private System.Windows.Forms.Label Detalle3;
+        private System.Windows.Forms.Label Detalle4;
+        private System.Windows.Forms.Label Detalle5;
+        private System.Windows.Forms.Button BotonAtrasVentana;
+        private System.Windows.Forms.Button BotonAnteriorComentario;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label IndexComentarioLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AutorTextBox;
+        private System.Windows.Forms.TextBox ComentarioTextBox;
+        private System.Windows.Forms.Button BotonAgregarComentario;
+        private System.Windows.Forms.Label Icono;
+    }
+}
+
